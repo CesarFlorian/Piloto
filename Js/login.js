0456cc67b0a143968cd0f1159847445b95e25b7a
Florian De La Cruz <cffloriandel@quad.com>
2026-06-23 14:58:17 -0400

    login correcto

---
+++ Js/login.js
@@ -0,0 +1,66 @@
+const users = [
+
+    {
+        username:"operator",
+        password:"1234",
+        role:"operator"
+    },
+    {
+       username:"manager",
+       password:"1234",
+       role:"manager"
+    }
+
+
+
+
+
+];
+
+const loginForm = document.getElementById("loginForm")
+
+loginForm.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const user = users.find (u => 
+
+        u.username === username &&
+        u.password === password
+    );
+
+    if(user){
+               
+        localStorage.setItem(
+            "currentUser",
+            JSON.stringify(user)
+
+        );
+
+    if(user.role === "operator")
+        {
+           window.location.href =
+              "pages/operator/dashboard.html"
+
+        }
+
+    if(user.role === "manager")
+        {
+           window.location.href =
+              "pages/manager/dashboard.html"
+
+        }
+    else{
+
+        document.getElementById("errorMessage").textContent = "Usuario o Contraseña incorrecto";
+
+               
+
+        }
+
+
+    }
+});
